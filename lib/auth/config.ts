5f5a87017368b5f8e818bd0f38dddc25dca55790
eamongordon <82300336+eamongordon@users.noreply.github.com>
2025-01-26 12:56:05 -0800

    fix: add @ts-expect-error to auth config

--- lib/auth/config.ts
+++ lib/auth/config.ts
@@ -1,7 +1,7 @@
 import { NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
-//@ts-ignore
+//@ts-expect-error
 import { JWT } from "next-auth/jwt";
 import { db, users } from "../schema";
 import { eq } from "drizzle-orm";
