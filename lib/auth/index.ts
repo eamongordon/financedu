3547fe8347542316720c932f35be916be8fe64cf
Eamon Gordon <ekeokigordon@icloud.com>
2025-01-25 18:58:45 -0800

    Revert "chore: create prisma instance" and "chore: add prisma orm"
    Reverts "88398135ec38290ce889f93c78df9b26e2e4ae4c" and "ebc7963e36bbefc4a5a4a5cf2faebb4f1be57a12"

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,8 +1,5 @@
 import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "@/prisma"
-
+ 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [],
+  providers: [],
 })
