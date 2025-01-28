44d6ea29d1c25c7fdd9fbabf80a5a65da4384319
eamongordon <82300336+eamongordon@users.noreply.github.com>
2025-01-28 09:31:40 -0800

    feat: use new auth layout for login and signup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import { Suspense } from "react"
+import Image from "next/image"
+
+export default function AuthLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="grid min-h-svh lg:grid-cols-2">
+            <div className="relative hidden bg-muted lg:block">
+                <div className="absolute inset-0 bg-gradient-to-br from-[#00B5EA] to-[#02CF46]"></div>
+                <Image
+                    src="/financedu-homepage_banner.jpeg"
+                    alt="Image"
+                    fill
+                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale opacity-20"
+                />
+            </div>
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        <Suspense>
+                            {children}
+                        </Suspense>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
