a65418de8a7cf445c746d48c28ff497a2829e9f1
eamongordon <82300336+eamongordon@users.noreply.github.com>
2025-02-01 14:49:37 -0800

    refactor: replace grid with flex for responsiveness in homepage hero

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -5,8 +5,8 @@ import { cn } from "@/lib/utils";
 
 export function Hero() {
     return (
-        <div className="grid min-h-dvh md:min-h-[550px] md:grid-cols-2">
-            <div className="relative">
+        <div className="flex flex-col h-dvh md:h-[550px] md:flex-row">
+            <div className="relative h-1/3 md:h-auto md:w-1/2">
                 <div className="absolute inset-0 bg-gradient-to-br from-[#00B5EA] to-[#02CF46] dark:brightness-[0.6]"></div>
                 <Image
                     src="/homepage-banner.jpg"
@@ -15,8 +15,8 @@ export function Hero() {
                     className="absolute inset-0 h-full w-full object-cover dark:brightness-75 opacity-20"
                 />
             </div>
-            <div className="flex flex-col justify-start items-start md:justify-center md:items-center gap-4 p-6 lg:p-10 bg-muted/60">
-                <div className="flex flex-col text-center md:text-left gap-6 md:gap-12">
+            <div className="h-2/3 md:h-auto md:w-1/2 flex flex-col justify-start items-start md:justify-center md:items-center gap-4 p-6 lg:p-10 bg-muted/60">
+                <div className="flex flex-col text-left md:text-left gap-6 md:gap-12">
                     <h1 className="text-[26px] md:text-left md:text-5xl font-semibold">Financial Education For Youth</h1>
                     <p className="text-lg text-muted-foreground">Empowering youth to live successful financial lives through online, cost-free, and customizable curricula.</p>
                     <div className="flex flex-row justify-center md:justify-normal flex-wrap gap-2 lg:gap-4">
