4aabaecff3a1dbb5db8123fc03848691f5d19e73
eamongordon <82300336+eamongordon@users.noreply.github.com>
2025-02-04 17:43:29 -0800

    feat: add default values to profile form

--- app/(account)/settings/page.tsx
+++ app/(account)/settings/page.tsx
@@ -1,7 +1,17 @@
-import { ProfileForm } from "@/components/account/profile-form";
+import { ProfileForm, type ProfileFormValues } from "@/components/account/profile-form";
+import { auth } from "@/lib/auth";
 
-export default function SettingsPage() {
+export default async function SettingsPage() {
+    const session = await auth();
+    if (!session?.user) {
+        throw new Error("Not authenticated");
+    }
+    const defaultValues = {
+        email: session.user?.email || "",
+        firstName: session.user?.firstName || "",
+        lastName: session.user?.lastName || "",
+    } as ProfileFormValues;
     return (
-        <ProfileForm />
+        <ProfileForm defaultValues={defaultValues} />
     )
 }
--- components/account/profile-form.tsx
+++ components/account/profile-form.tsx
@@ -16,6 +16,8 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { editUser } from "@/lib/actions"
+import { useRouter } from "next/navigation";
+import { SessionProvider, useSession } from "next-auth/react";
 
 const profileFormSchema = z.object({
     email: z.string().email(),
@@ -23,21 +25,35 @@ const profileFormSchema = z.object({
     lastName: z.string().optional(),
 })
 
-type ProfileFormValues = z.infer<typeof profileFormSchema>
+export type ProfileFormValues = z.infer<typeof profileFormSchema>
 
-export function ProfileForm() {
+interface ProfileFormProps {
+    defaultValues: ProfileFormValues;
+}
+
+export function ProfileForm({ defaultValues }: ProfileFormProps) {
+    return (
+        <SessionProvider>
+            <ProfileFormInner defaultValues={defaultValues} />
+        </SessionProvider>
+    )
+}
+
+export function ProfileFormInner({ defaultValues }: ProfileFormProps) {
+    const router = useRouter();
+    const { update } = useSession();
+    
     const form = useForm<ProfileFormValues>({
         resolver: zodResolver(profileFormSchema),
-        defaultValues: {
-            firstName: "",
-            lastName: "",
-            email: "",
-        },
+        defaultValues,
     })
 
     async function onSubmit(data: ProfileFormValues) {
         try {
+
             await editUser(data);
+            await update(data);
+            router.refresh();
             toast.success("Profile updated!");
         } catch (error) {
             console.error(error);
@@ -46,49 +62,49 @@ export function ProfileForm() {
     }
 
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Email" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="firstName"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>First Name</FormLabel>
-                            <FormControl>
-                                <Input placeholder="First Name" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="lastName"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Last Name</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Last Name" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Update profile</Button>
-            </form>
-        </Form>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Email" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="firstName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>First Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="First Name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="lastName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Last Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Last Name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Update profile</Button>
+                </form>
+            </Form>
     )
 }
