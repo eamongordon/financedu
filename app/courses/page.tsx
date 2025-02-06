412dc657673b27afb126413e79e4c22b36db6286
Eamon Gordon <ekeokigordon@icloud.com>
2025-02-05 22:16:26 -0800

    feat: enhance Courses page with card layout for course listings

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,17 +1,42 @@
 import { listCourses } from "@/lib/actions";
 import Banner from "@/components/banner";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export default async function CoursesPage() {
     const courses = await listCourses();
 
     return (
         <div>
-            <Banner title="Courses"/>
-            <ul>
+            <Banner title="Courses" />
+            <ul className="flex justify-center items-center flex-wrap gap-4 my-10">
                 {courses.map((course) => (
                     <li key={course.id}>
-                        <h2>{course.title}</h2>
-                        <p>{course.description}</p>
+                        <Card className="w-[300px] sm:w-[375px] rounded-lg">
+                            <div className="relative w-full h-48">
+                                <Image src={"/homepage-banner.jpg"} alt={course.title} layout="fill" className="object-cover rounded-t-lg" />
+                            </div>
+                            <CardHeader>
+                                <CardTitle>{course.title}</CardTitle>
+                            </CardHeader>
+                            <CardContent className="flex flex-col gap-6">
+                                <p>{course.description}</p>
+                                <div className="flex gap-10 sm:gap-12">
+                                    <div>
+                                        <p className="font-semibold">Length (Hrs.):</p>
+                                        <p className="text-2xl">32 - 56</p>
+                                    </div>
+                                    <div>
+                                        <p className="font-semibold">Grade Levels:</p>
+                                        <p className="text-2xl">7 - 12</p>
+                                    </div>
+                                </div>
+                                <Link href={`/courses/${course.id}`} className={cn(buttonVariants(), "w-full")}>View Course</Link>
+                            </CardContent>
+                        </Card>
                     </li>
                 ))}
             </ul>
