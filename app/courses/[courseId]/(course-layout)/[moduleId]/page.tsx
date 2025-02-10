650846b1e7679e964e8bedefc38a823df745bb0a
eamongordon <82300336+eamongordon@users.noreply.github.com>
2025-02-10 12:32:11 -0800

    feat: add link to lesson in module page

--- app/courses/[courseId]/(course-layout)/[moduleId]/page.tsx
+++ app/courses/[courseId]/(course-layout)/[moduleId]/page.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
 import { getModuleWithLessonsAndActivities } from "@/lib/actions";
+import Link from "next/link";
 
 export default async function CourseLayout({
     params,
@@ -22,7 +23,7 @@ export default async function CourseLayout({
                         <p dangerouslySetInnerHTML={{ __html: lesson.objectives ?? "" }} />
                     </CardContent>
                     <CardFooter>
-                        <Button>Get Started</Button>
+                        <Link className={buttonVariants()} href={`/courses/${moduleObj.courseId}/${moduleObj.id}/${lesson.id}`}>Get Started</Link>
                     </CardFooter>
                 </Card>
             ))}
