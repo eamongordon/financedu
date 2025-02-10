307a5d812c0be40a59fc5681845578e0a9486260
eamongordon <82300336+eamongordon@users.noreply.github.com>
2025-02-10 13:48:12 -0800

    fix: adjust height calculation for scrolling in course layout

--- app/courses/[courseId]/(course-layout)/layout.tsx
+++ app/courses/[courseId]/(course-layout)/layout.tsx
@@ -16,7 +16,7 @@ export default async function CourseLayout({
             className="w-full flex flex-col sm:flex-row sm:flex-grow sm:divide-x divide-muted"
         >
             <div
-                className="py-4 sm:w-1/3 flex flex-col gap-4 items-start px-2 sm:px-8 text-left sm:sticky top-[64px] mb-4 sm:mb-0 sm:h-[calc(100vh-96px)] overflow-auto"//todo: figure out proper height for scolling
+                className="py-4 sm:w-1/3 flex flex-col gap-4 items-start px-2 sm:px-8 text-left sm:sticky top-[64px] mb-4 sm:mb-0 sm:h-[calc(100vh-64px)] overflow-auto"//todo: figure out proper height for scolling
             >
                 <h1 className="text-2xl font-bold">{course.title}</h1>
                 <p className="mt-2">{course.description}</p>
