78c41c2df51b3e017a3b33c049ddef1344719b0d
eamongordon <82300336+eamongordon@users.noreply.github.com>
2025-02-10 17:55:27 -0800

    feat: add article content display

--- app/courses/[courseId]/[moduleId]/[lessonId]/[activityId]/page.tsx
+++ app/courses/[courseId]/[moduleId]/[lessonId]/[activityId]/page.tsx
@@ -8,6 +8,14 @@ export default async function LessonPage({
     const { activityId } = await params;
     const activity = await getActivity(activityId);
     return (
-        <p>Activity: {activity.title}</p>
+        <main>
+            {activity.type === "Article" && (
+                <div className="flex flex-col gap-4">
+                    <h1 className="font-semibold text-xl md:text-2xl lg:text-4xl">{activity.title}</h1>
+                    <div dangerouslySetInnerHTML={{ __html: activity.content! }} />
+                </div>
+            )}
+            <p>Activity: {activity.title}</p>
+        </main>
     );
 }
