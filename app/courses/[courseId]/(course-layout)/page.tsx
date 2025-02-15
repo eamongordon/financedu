1b89e1bc48f2e5d3d4ed2abae5ca0b07132a2a35
Eamon Gordon <ekeokigordon@icloud.com>
2025-02-15 10:19:47 -0800

    feat: add module display to course page

--- app/courses/[courseId]/(course-layout)/page.tsx
+++ app/courses/[courseId]/(course-layout)/page.tsx
@@ -1,4 +1,10 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getCourseWithModulesAndLessons } from "@/lib/actions"
+import { cn } from "@/lib/utils";
+import { GraduationCap } from "lucide-react";
+import { DynamicIcon, dynamicIconImports } from "lucide-react/dynamic";
+import Link from "next/link";
 
 export default async function Page({
     params,
@@ -9,6 +15,53 @@ export default async function Page({
     const course = await getCourseWithModulesAndLessons(slug);
     console.log("course", course);
     return (
-        <p>Course Overview</p>
+        <main>
+            <section className="flex flex-col gap-2 md:gap-6 pb-2 md:pb-6 border-b">
+                <p>{course.description}</p>
+                <div className="flex flex-row gap-10 md:gap-12">
+                    <div>
+                        <p className="font-semibold">Length (Hrs.):</p>
+                        <p className="text-muted-foreground text-2xl">32 - 56</p>
+                    </div>
+                    <div>
+                        <p className="font-semibold">Grade Levels:</p>
+                        <p className="text-muted-foreground text-2xl">7 - 12</p>
+                    </div>
+                </div>
+            </section>
+            <section className="pt-2 md:pt-8">
+                {course.modules.map(module => (
+                    <Card key={module.id} className="mb-4">
+                        <CardHeader className="flex flex-row items-center gap-2 space-y-0">
+                            <div className="border flex justify-center items-center size-8 shrink-0 rounded-md">
+                                {module.icon ? <DynamicIcon name={module.icon as keyof typeof dynamicIconImports} strokeWidth={1.5} /> : <GraduationCap strokeWidth={1.5} />}
+                            </div>
+                            <Link
+                                href={`/courses/${course.id}/${module.id}`}
+                                className={cn(buttonVariants({ variant: "link" }), "text-card-foreground")}
+                            >
+                                <CardTitle className="text-xl font-semibold leading-none">{module.title}</CardTitle>
+                            </Link>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="grid grid-cols-1 lg:grid-cols-2">
+                                {module.lessons.map((lesson) => (
+                                    <Link
+                                        key={lesson.id}
+                                        href={`/courses/${course.id}/${module.id}/${lesson.id}`}
+                                        className={cn(
+                                            buttonVariants({ variant: "link" }),
+                                            "text-muted-foreground [&_svg]:size-4 whitespace-normal justify-start",
+                                        )}
+                                    >
+                                        {lesson.title}
+                                    </Link>
+                                ))}
+                            </div>
+                        </CardContent>
+                    </Card>
+                ))}
+            </section>
+        </main>
     );
 }
