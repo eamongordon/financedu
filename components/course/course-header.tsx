6746f5d17860775124a1e9c774dc447c6b0aac54
Eamon Gordon <ekeokigordon@icloud.com>
2025-02-15 09:46:14 -0800

    feat: enhance CourseHeader with gradient background image overlay

--- components/course/course-header.tsx
+++ components/course/course-header.tsx
@@ -17,7 +17,10 @@ export function CourseHeader({ course }: { course: getCourseWithModulesAndLesson
             href={`/courses/${course.id}`}
             className={cn(buttonVariants({ variant: "ghost" }), "w-full h-auto rounded-none py-4 border-l-4 border-l-transparent flex flex-row justify-start items-center gap-4 px-2 sm:px-6", isOnModulePage ? "border-l-transparent" : "border-l-primary")}
         >
-            <Image src={course.image ?? "/homepage-banner.jpg"} alt={course.title} width={64} height={64} className="rounded-lg" />
+            <div className="relative">
+                <div className="absolute rounded-lg inset-0 bg-gradient-to-br from-[#00B5EA] to-[#02CF46] dark:brightness-[0.6]"></div>
+                <Image src={course.image ?? "/homepage-banner.jpg"} alt={course.title} width={64} height={64} className="rounded-lg before:absolute before:inset-0 before:bg-black before:opacity-50 before:rounded-lg before:w-10 before:h-10 opacity-10" />
+            </div>
             <div className="flex flex-col justify-center gap-2">
                 <h1 className="leading-none text-xl font-bold">{course.title}</h1>
                 <h4 className="leading-none font-semibold text-secondary">{course.modules.length} Module{course.modules.length === 1 ? "" : "s"}</h4>
