d752f4cab0aabae05400ca37a31b376d1d0ff629
Eamon Gordon <ekeokigordon@icloud.com>
2025-02-16 15:36:44 -0800

    fix: correct isComplete ts error in lesson layout

--- app/courses/[courseId]/[moduleId]/[lessonId]/layout.tsx
+++ app/courses/[courseId]/[moduleId]/[lessonId]/layout.tsx
@@ -6,33 +6,27 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { auth } from "@/lib/auth";
 
-export default async function LessonLayout({
-    params,
-    children,
-}: {
+interface LessonLayoutProps {
     params: Promise<{ courseId: string, moduleId: string, lessonId: string }>,
     children: React.ReactNode
-}) {
+}
+
+type LessonWithActivitiesAndUserProgress = Awaited<ReturnType<typeof getLessonWithActivitiesAndUserProgress>>;
+
+export default async function LessonLayout({ params, children }: LessonLayoutProps) {
     const { courseId, moduleId, lessonId } = await params;
     const nextLesson = await getNextLesson(lessonId);
     const previousLesson = await getPreviousLesson(lessonId);
     const session = await auth();
 
-    let lesson: Awaited<ReturnType<typeof getLessonWithActivitiesAndUserProgress>> | Awaited<ReturnType<typeof getLessonWithActivities>>;
-    const isLoggedIn = session && session.user && session.user.id
-    if (isLoggedIn) {
-        lesson = await getLessonWithActivitiesAndUserProgress(lessonId);
-    } else {
-        lesson = await getLessonWithActivities(lessonId);
-    }
+    const isLoggedIn = session && session.user && session.user.id;
+    const lesson = isLoggedIn
+        ? await getLessonWithActivitiesAndUserProgress(lessonId)
+        : await getLessonWithActivities(lessonId);
 
     return (
-        <div
-            className="w-full flex flex-col sm:flex-row sm:flex-grow sm:divide-x divide-border"
-        >
-            <div
-                className="sm:w-1/3 flex flex-col gap-4 items-start text-left sm:sticky top-[64px] mb-4 sm:mb-0 sm:h-[calc(100vh-64px)] overflow-auto"
-            >
+        <div className="w-full flex flex-col sm:flex-row sm:flex-grow sm:divide-x divide-border">
+            <div className="sm:w-1/3 flex flex-col gap-4 items-start text-left sm:sticky top-[64px] mb-4 sm:mb-0 sm:h-[calc(100vh-64px)] overflow-auto">
                 <div className="py-4 w-full border-b flex flex-row items-center gap-4 px-2 md:px-8">
                     <div className="size-12 aspect-square bg-gradient-to-br from-[#00B5EA] to-[#02CF46] dark:from-[#00B5EA]/60 dark:to-[#02CF46]/60 rounded-lg flex justify-center items-center text-background">
                         <ChartLine />
@@ -47,37 +41,31 @@ export default async function LessonLayout({
                     </div>
                 </div>
                 <div className="w-full flex flex-row justify-between items-center px-2 md:px-8 py-3">
-                    {previousLesson.lesson ?
+                    {previousLesson.lesson ? (
                         <Link
                             href={`/courses/${courseId}/${moduleId}/${previousLesson.lesson.id}`}
                             className={cn(buttonVariants({ variant: "ghost" }), "[&_svg]:size-6 p-0")}
                         >
                             <ChevronLeft className="text-muted-foreground" />
-                        </Link> :
-                        <Button
-                            disabled
-                            className="[&_svg]:size-6 p-0"
-                            variant="ghost"
-                        >
+                        </Link>
+                    ) : (
+                        <Button disabled className="[&_svg]:size-6 p-0" variant="ghost">
                             <ChevronLeft className="text-muted-foreground" />
                         </Button>
-                    }
+                    )}
                     <h1 className="mx-2 text-lg font-bold text-center">{lesson.title}</h1>
-                    {nextLesson.lesson ?
+                    {nextLesson.lesson ? (
                         <Link
                             href={`/courses/${courseId}/${moduleId}/${nextLesson.lesson.id}`}
                             className={cn(buttonVariants({ variant: "ghost" }), "[&_svg]:size-6 p-0")}
                         >
                             <ChevronRight className="text-muted-foreground" />
-                        </Link> :
-                        <Button
-                            disabled
-                            className="[&_svg]:size-6 p-0"
-                            variant="ghost"
-                        >
+                        </Link>
+                    ) : (
+                        <Button disabled className="[&_svg]:size-6 p-0" variant="ghost">
                             <ChevronRight className="text-muted-foreground" />
                         </Button>
-                    }
+                    )}
                 </div>
                 <div className="w-full">
                     <ActivityNav activities={lesson.lessonToActivities.map(lessonToActivitiesObj => ({
@@ -85,15 +73,13 @@ export default async function LessonLayout({
                         type: lessonToActivitiesObj.activity.type,
                         title: lessonToActivitiesObj.activity.title,
                         href: `/courses/${courseId}/${moduleId}/${lessonId}/${lessonToActivitiesObj.activity.id}`,
-                        isComplete: isLoggedIn && 'userProgress' in lessonToActivitiesObj.activity ? lessonToActivitiesObj.activity.userProgress.some(userProgress => userProgress.activityId === lessonToActivitiesObj.activity.id && userProgress.completedAt) : undefined,
+                        isComplete: isLoggedIn ? (lessonToActivitiesObj as LessonWithActivitiesAndUserProgress["lessonToActivities"][number]).activity.userCompletion.some(userProgress => userProgress.activityId === lessonToActivitiesObj.activity.id && userProgress.completedAt) : undefined,
                     }))} />
                 </div>
             </div>
-            <div
-                className="sm:w-2/3 flex flex-col items-center mx-auto overflow-auto"
-            >
+            <div className="sm:w-2/3 flex flex-col items-center mx-auto overflow-auto">
                 {children}
             </div>
-        </div >
+        </div>
     );
 }
