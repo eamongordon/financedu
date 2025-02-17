c04bc53a8f8d4a46f042a069fc815226094623b9
Eamon Gordon <ekeokigordon@icloud.com>
2025-02-17 09:45:12 -0800

    feat: add user progress and activity links to lesson page

--- app/courses/[courseId]/[moduleId]/[lessonId]/page.tsx
+++ app/courses/[courseId]/[moduleId]/[lessonId]/page.tsx
@@ -1,14 +1,66 @@
-import { getLessonWithActivities } from "@/lib/actions";
+import { buttonVariants } from "@/components/ui/button";
+import { getLessonWithActivities, getLessonWithActivitiesAndUserProgress } from "@/lib/actions";
+import { auth } from "@/lib/auth";
+import { cn } from "@/lib/utils";
+import Link from "next/link"
+import { FileText, CircleHelp, Check } from "lucide-react";
+
+type LessonWithActivitiesAndUserProgress = Awaited<ReturnType<typeof getLessonWithActivitiesAndUserProgress>>;
 
 export default async function LessonPage({
     params,
 }: {
-    params: Promise<{ lessonId: string }>,
+    params: Promise<{ courseId: string, moduleId: string, lessonId: string }>,
 }) {
+    const courseId = (await params).courseId;
+    const moduleId = (await params).moduleId;
     const lessonId = (await params).lessonId;
-    const lesson = await getLessonWithActivities(lessonId);
+    const session = await auth();
+    const isLoggedIn = session && session.user && session.user.id;
+    const lesson = isLoggedIn ?
+        await getLessonWithActivitiesAndUserProgress(lessonId) :
+        await getLessonWithActivities(lessonId);
 
+    console.log("lesson", lesson)
     return (
-        <p>Viewing Lesson Overview: {lesson.title}</p>
+        <main className="p-2 sm:p-8">
+            <section className="flex justify-between gap-2 md:gap-6 pb-2 md:pb-6 border-b">
+                <div>
+                    <p className="font-semibold">Description:</p>
+                    <p>{lesson.description}</p>
+                </div>
+                <div>
+                    <p className="font-semibold">Objectives:</p>
+                    <p dangerouslySetInnerHTML={{ __html: lesson.objectives ?? "" }} />
+                </div>
+            </section>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                {lesson.lessonToActivities.map((lessonToActivitiesObj) => (
+                    <Link
+                        key={lessonToActivitiesObj.activity.id}
+                        href={`/courses/${courseId}/${moduleId}/${lessonId}/${lessonToActivitiesObj.activity.id}`}
+                        className={cn(
+                            buttonVariants({ variant: "link" }),
+                            "py-8 text-base text-foreground [&_svg]:size-4 whitespace-normal justify-start",
+                        )}
+                    >
+                        <div className="relative">
+                            <div className={
+                                cn("border flex justify-center items-center size-8 shrink-0 rounded-md mr-4 relative",
+                                    isLoggedIn && lessonToActivitiesObj.activity ? "border-primary text-primary" : "")}
+                            >
+                                {lessonToActivitiesObj.activity.type === "Article" ? <FileText strokeWidth={1.5} /> : <CircleHelp strokeWidth={1.5} />}
+                                {isLoggedIn && (lessonToActivitiesObj as LessonWithActivitiesAndUserProgress["lessonToActivities"][number]).activity.userCompletion.length > 0 && (
+                                    <div className="text-white absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-primary rounded-full size-4 flex items-center justify-center [&_svg]:size-[11px]">
+                                        <Check />
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        {lessonToActivitiesObj.activity.title}
+                    </Link>
+                ))}
+            </div>
+        </main>
     );
 }
