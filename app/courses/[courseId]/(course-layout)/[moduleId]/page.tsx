afc2e979e86ee9f782371f02f9f803f691dd1767
Eamon Gordon <ekeokigordon@icloud.com>
2025-02-17 09:58:36 -0800

    fix: add type safety for moduleId page

--- app/courses/[courseId]/(course-layout)/[moduleId]/page.tsx
+++ app/courses/[courseId]/(course-layout)/[moduleId]/page.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 import { Check, CircleHelp, FileText } from "lucide-react";
 import Link from "next/link";
 
+type ModuleWithLessonsAndActivitiesAndUserCompletion = Awaited<ReturnType<typeof getModuleWithLessonsAndActivitiesAndUserCompletion>>;
+
 export default async function CourseLayout({
     params,
 }: {
@@ -40,10 +42,10 @@ export default async function CourseLayout({
                                     <div className="relative">
                                         <div className={
                                             cn("border flex justify-center items-center size-8 shrink-0 rounded-md mr-4 relative",
-                                                isLoggedIn && lessonToActivitiesObj.activity.userCompletion.length ? "border-primary text-primary" : "")}
+                                                isLoggedIn && (lessonToActivitiesObj as ModuleWithLessonsAndActivitiesAndUserCompletion["lessons"][number]["lessonToActivities"][number]).activity.userCompletion.length ? "border-primary text-primary" : "")}
                                         >
                                             {lessonToActivitiesObj.activity.type === "Article" ? <FileText strokeWidth={1.5} /> : <CircleHelp strokeWidth={1.5} />}
-                                            {isLoggedIn && lessonToActivitiesObj.activity.userCompletion.length > 0 && (
+                                            {isLoggedIn && (lessonToActivitiesObj as ModuleWithLessonsAndActivitiesAndUserCompletion["lessons"][number]["lessonToActivities"][number]).activity.userCompletion.length > 0 && (
                                                 <div className="text-white absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-primary rounded-full size-4 flex items-center justify-center [&_svg]:size-[11px]">
                                                     <Check />
                                                 </div>
