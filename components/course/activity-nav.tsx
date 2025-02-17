d6ca0fb6fdd650c9703c3032ffebbd8d4355ec41
Eamon Gordon <ekeokigordon@icloud.com>
2025-02-16 19:38:33 -0800

    feat: track browsed articles in activity navigation

--- components/course/activity-nav.tsx
+++ components/course/activity-nav.tsx
@@ -6,6 +6,7 @@ import { useSelectedLayoutSegments } from "next/navigation"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "../ui/button"
 import { Check, CircleHelp, FileText } from "lucide-react"
+import { useEffect, useState } from "react"
 
 interface ActivityNavProps extends React.HTMLAttributes<HTMLElement> {
   activities: {
@@ -20,7 +21,14 @@ interface ActivityNavProps extends React.HTMLAttributes<HTMLElement> {
 export function ActivityNav({ className, activities, ...props }: ActivityNavProps) {
   const segments = useSelectedLayoutSegments()
   const currentActivityId = segments![segments!.length - 1]
+  const [browsedArticles, setBrowsedArticles] = useState<string[]>([]);
 
+  useEffect(() => {
+    if (currentActivityId && activities.find(activity => activity.id === currentActivityId)?.type === "Article") {
+      setBrowsedArticles(prev => [...prev, currentActivityId]);
+    }
+  }, [currentActivityId, activities]);
+  
   return (
     <nav
       className={cn(
@@ -46,10 +54,10 @@ export function ActivityNav({ className, activities, ...props }: ActivityNavProp
             <div className={
               cn("border flex justify-center items-center size-8 shrink-0 rounded-md mr-4 relative",
                 currentActivityId === activity.id ? "dark:border-muted-foreground" : "",
-                activity.isComplete ? "border-primary text-primary" : "")}
+                activity.isComplete || browsedArticles.includes(activity.id) ? "border-primary text-primary" : "")}
             >
               {activity.type === "Article" ? <FileText strokeWidth={1.5} /> : <CircleHelp strokeWidth={1.5} />}
-              {activity.isComplete && (
+              {(activity.isComplete || browsedArticles.includes(activity.id)) && (
                 <div className="text-white absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-primary rounded-full size-4 flex items-center justify-center [&_svg]:size-3">
                   <Check />
                 </div>
