d606ece5a4a9ca3056d5dd305813fb5233e22fc0
Eamon Gordon <ekeokigordon@icloud.com>
2025-03-03 20:39:48 -0800

    refactor: improve visbility of completion icon dark mode

--- components/ui/completion-icon.tsx
+++ components/ui/completion-icon.tsx
@@ -12,14 +12,13 @@ export function CompletionIcon({ isComplete, icon, isCurrent }: CompletionIconPr
     <div className="relative">
       <div className={
         cn("border flex justify-center items-center size-8 shrink-0 rounded-md relative",
-          isCurrent ? "dark:border-muted-foreground" : "",
-          isComplete ? "border-primary text-primary" : "")}
+          isComplete ? "border-primary text-primary" : isCurrent ? "dark:border-muted-foreground" : "")}
       >
         {icon}
         {isComplete && (
           <div className="text-white absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-primary rounded-full size-4 flex items-center justify-center [&_svg]:size-[11px]">
-          <Check />
-        </div>
+            <Check />
+          </div>
         )}
       </div>
     </div>
