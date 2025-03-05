46e23aa3ef6bc41807ceaaa805ccc4c57fda6e54
Eamon Gordon <ekeokigordon@icloud.com>
2025-03-04 19:39:30 -0800

    refactor: improve create assignment layout

--- components/account/create-assigments.tsx
+++ components/account/create-assigments.tsx
@@ -8,6 +8,7 @@ import {
     Dialog,
     DialogContent,
     DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
@@ -26,12 +27,19 @@ import { Plus } from "lucide-react"
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "@/components/ui/accordion"
 import { Checkbox } from "@/components/ui/checkbox"
 import { type getCoursesWithModulesAndLessonsAndActivities } from "@/lib/actions";
+import { cn } from "@/lib/utils";
 
 type CoursesWithModulesAndLessonsAndActivities = Awaited<ReturnType<typeof getCoursesWithModulesAndLessonsAndActivities>>
 
 export function CreateAssignments({ isNoChildren, courses }: { isNoChildren?: boolean, courses: CoursesWithModulesAndLessonsAndActivities }) {
     const [open, setOpen] = React.useState(false)
+    const [selectedActivities, setSelectedActivities] = React.useState<string[]>([])
     const isDesktop = useMediaQuery("(min-width: 768px)")
+
+    const handleSubmit = () => {
+        console.log(selectedActivities);
+        setOpen(false);
+    };
 
     if (isDesktop) {
         return (
@@ -42,14 +50,19 @@ export function CreateAssignments({ isNoChildren, courses }: { isNoChildren?: bo
                         Create Assignments
                     </Button>
                 </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px] max-h-[calc(100dvh-100px)] overflow-auto">
-                    <DialogHeader>
+                <DialogContent className="flex flex-col max-h-[calc(100dvh-64px)]">
+                    <DialogHeader className="sticky top-0 bg-background">
                         <DialogTitle>Assign Content</DialogTitle>
                         <DialogDescription>
                             Assign Lessons
                         </DialogDescription>
                     </DialogHeader>
-                    <ContentSelector setOpen={setOpen} courses={courses} />
+                    <ContentSelector courses={courses} selectedActivities={selectedActivities} setSelectedActivities={setSelectedActivities} />
+                    <DialogFooter>
+                        <Button disabled={selectedActivities.length === 0} onClick={() => handleSubmit()}>
+                            ({selectedActivities.length > 0 ? "Assign " + selectedActivities.length : "Assign"})
+                        </Button>
+                    </DialogFooter>
                 </DialogContent>
             </Dialog>
         )
@@ -63,15 +76,18 @@ export function CreateAssignments({ isNoChildren, courses }: { isNoChildren?: bo
                     Create Assignments
                 </Button>
             </DrawerTrigger>
-            <DrawerContent className="max-h-[calc(100dvh-100px)] overflow-auto">
+            <DrawerContent className="max-h-[calc(100dvh-64px)]">
                 <DrawerHeader className="text-left">
                     <DrawerTitle>Assign Content</DrawerTitle>
                     <DrawerDescription>
                         Assign lessons.
                     </DrawerDescription>
                 </DrawerHeader>
-                <ContentSelector className="px-4" setOpen={setOpen} courses={courses} />
+                <ContentSelector className="px-4" courses={courses} selectedActivities={selectedActivities} setSelectedActivities={setSelectedActivities} />
                 <DrawerFooter className="pt-2">
+                    <Button disabled={selectedActivities.length === 0} onClick={() => handleSubmit()}>
+                        ({selectedActivities.length > 0 ? "Assign " + selectedActivities.length : "Assign"})
+                    </Button>
                     <DrawerClose asChild>
                         <Button variant="outline">Cancel</Button>
                     </DrawerClose>
@@ -81,9 +97,7 @@ export function CreateAssignments({ isNoChildren, courses }: { isNoChildren?: bo
     )
 }
 
-function ContentSelector({ className, setOpen, courses }: { className?: string, setOpen: React.Dispatch<React.SetStateAction<boolean>>, courses: CoursesWithModulesAndLessonsAndActivities }) {
-    const [selectedActivities, setSelectedActivities] = React.useState<string[]>([])
-
+function ContentSelector({ className, courses, selectedActivities, setSelectedActivities }: { className?: string, courses: CoursesWithModulesAndLessonsAndActivities, selectedActivities: string[], setSelectedActivities: React.Dispatch<React.SetStateAction<string[]>> }) {
     const handleCheckboxChange = (isChecked: boolean, type: string, id: string) => {
         let scopedActivities = [];
         if (type === 'course') {
@@ -102,51 +116,52 @@ function ContentSelector({ className, setOpen, courses }: { className?: string, 
         }
     }
 
-    const handleSubmit = () => {
-        console.log(selectedActivities);
-        setOpen(false);
-    };
-
     return (
-        <div className={className}>
-            <Accordion type="multiple">
+        <div className={cn("flex-1 overflow-y-auto", className)}>
+            <Accordion type="multiple" className="w-full">
                 {courses.map(course => (
                     <AccordionItem key={course.id} value={course.id}>
-                        <Checkbox
-                            checked={course.modules.every(module => module.lessons.every(lesson => lesson.activities.every(activity => selectedActivities.includes(activity.id))))}
-                            onCheckedChange={(isChecked) => handleCheckboxChange(!!isChecked, 'course', course.id)}
-                        />
-                        <AccordionTrigger>
-                            {course.title}
-                        </AccordionTrigger>
+                        <div className="flex flex-row items-center justify-between">
+                            <AccordionTrigger className="justify-end flex-row-reverse gap-4">
+                                {course.title}
+                            </AccordionTrigger>
+                            <Checkbox
+                                checked={course.modules.every(module => module.lessons.every(lesson => lesson.activities.every(activity => selectedActivities.includes(activity.id))))}
+                                onCheckedChange={(isChecked) => handleCheckboxChange(!!isChecked, 'course', course.id)}
+                            />
+                        </div>
                         <AccordionContent>
                             {course.modules.map(module => (
-                                <AccordionItem key={module.id} value={module.id}>
-                                    <Checkbox
-                                        checked={module.lessons.every(lesson => lesson.activities.every(activity => selectedActivities.includes(activity.id)))}
-                                        onCheckedChange={(isChecked) => handleCheckboxChange(!!isChecked, 'module', module.id)}
-                                    />
-                                    <AccordionTrigger>
-                                        {module.title}
-                                    </AccordionTrigger>
+                                <AccordionItem key={module.id} value={module.id} className="pl-4">
+                                    <div className="flex flex-row items-center justify-between">
+                                        <AccordionTrigger className="justify-end flex-row-reverse gap-4">
+                                            {module.title}
+                                        </AccordionTrigger>
+                                        <Checkbox
+                                            checked={module.lessons.every(lesson => lesson.activities.every(activity => selectedActivities.includes(activity.id)))}
+                                            onCheckedChange={(isChecked) => handleCheckboxChange(!!isChecked, 'module', module.id)}
+                                        />
+                                    </div>
                                     <AccordionContent>
                                         {module.lessons.map(lesson => (
-                                            <AccordionItem key={lesson.id} value={lesson.id}>
-                                                <Checkbox
-                                                    checked={lesson.activities.every(activity => selectedActivities.includes(activity.id))}
-                                                    onCheckedChange={(isChecked) => handleCheckboxChange(!!isChecked, 'lesson', lesson.id)}
-                                                />
-                                                <AccordionTrigger>
-                                                    {lesson.title}
-                                                </AccordionTrigger>
+                                            <AccordionItem key={lesson.id} value={lesson.id} className="pl-4">
+                                                <div className="flex flex-row items-center justify-between">
+                                                    <AccordionTrigger className="justify-end flex-row-reverse gap-4">
+                                                        {lesson.title}
+                                                    </AccordionTrigger>
+                                                    <Checkbox
+                                                        checked={lesson.activities.every(activity => selectedActivities.includes(activity.id))}
+                                                        onCheckedChange={(isChecked) => handleCheckboxChange(!!isChecked, 'lesson', lesson.id)}
+                                                    />
+                                                </div>
                                                 <AccordionContent>
                                                     {lesson.activities.map(activity => (
-                                                        <div key={activity.id}>
+                                                        <div key={activity.id} className="flex flex-row items-center justify-between pl-4">
+                                                            {activity.title}
                                                             <Checkbox
                                                                 checked={selectedActivities.includes(activity.id)}
                                                                 onCheckedChange={(isChecked) => handleCheckboxChange(!!isChecked, 'activity', activity.id)}
                                                             />
-                                                            {activity.title}
                                                         </div>
                                                     ))}
                                                 </AccordionContent>
@@ -159,9 +174,6 @@ function ContentSelector({ className, setOpen, courses }: { className?: string, 
                     </AccordionItem>
                 ))}
             </Accordion>
-            <Button disabled={selectedActivities.length === 0} onClick={() => handleSubmit()}>
-                ({selectedActivities.length > 0 ? "Assign " + selectedActivities.length : "Assign"})
-            </Button>
         </div>
     )
 }
