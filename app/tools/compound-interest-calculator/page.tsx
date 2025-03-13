fdf89521024a3aa8c45f43fc31dd11c6c4e3b59e
Eamon Gordon <ekeokigordon@icloud.com>
2025-03-13 10:20:30 -0700

    feat: add breadcrumbs feature to banner

--- app/tools/compound-interest-calculator/page.tsx
+++ app/tools/compound-interest-calculator/page.tsx
@@ -1,10 +1,15 @@
 import Banner from "@/components/banner";
 import { CompoundInterestForm } from "@/components/tools/compound-interest-form";
 
+const breadcrumbs = [
+    { href: "/tools", label: "Tools" },
+    { href: "/tools/compound-interest-calculator", label: "Compound Interest Calculator" },
+];
+
 export default function CompoundInterestCalculatorPage() {
     return (
         <div>
-            <Banner title="Compound Interest Calculator"/>
+            <Banner title="Compound Interest Calculator" breadcrumbs={breadcrumbs}/>
             <CompoundInterestForm />
         </div>
     );
--- components/banner.tsx
+++ components/banner.tsx
@@ -1,15 +1,23 @@
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from './ui/breadcrumb';
+
+interface Breadcrumb {
+    href: string;
+    label: string;
+}
 
 interface BannerProps {
     title: string;
     backgroundImage?: string;
     className?: string;
+    breadcrumbs?: Breadcrumb[];
 }
 
-export default function Banner({ title, backgroundImage, className }: BannerProps) {
+export default function Banner({ title, backgroundImage, className, breadcrumbs }: BannerProps) {
     return (
-        <div className="relative flex justify-center items-center w-full h-24 sm:h-40">
+        <div className={cn("relative flex flex-col justify-center items-center w-full h-24 sm:h-40", breadcrumbs && "h-32")}>
             <div className={cn("absolute inset-0 bg-gradient-to-br from-[#00D9AE] to-[#3BDE2C] dark:brightness-[0.6]", className)} />
             <Image
                 src={backgroundImage ?? "/homepage-banner.jpg"}
@@ -17,7 +25,29 @@ export default function Banner({ title, backgroundImage, className }: BannerProp
                 layout="fill"
                 className="absolute inset-0 object-cover dark:brightness-75 opacity-10"
             />
-            <h1 className="text-white text-4xl sm:text-6xl font-bold z-10">{title}</h1>
+            {breadcrumbs && (
+                <Breadcrumb className="absolute top-4 left-4 sm:left-8 z-10">
+                    <BreadcrumbList className="text-neutral-100">
+                        {breadcrumbs.map((breadcrumb, index) => (
+                            index < breadcrumbs.length - 1 ? (
+                                <>
+                                    <BreadcrumbItem key={index}>
+                                        <BreadcrumbLink className="hover:text-white" asChild>
+                                            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+                                        </BreadcrumbLink>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator key={`separator-${index}`} />
+                                </>
+                            ) : (
+                                <BreadcrumbItem key={index}>
+                                    <BreadcrumbPage className="text-white">{breadcrumb.label}</BreadcrumbPage>
+                                </BreadcrumbItem>
+                            )
+                        ))}
+                    </BreadcrumbList>
+                </Breadcrumb>
+            )}
+            <h1 className={cn("text-white font-bold text-center z-10", breadcrumbs ? "text-2xl sm:text-4xl mt-6 sm:mt-2" : "text-4xl sm:text-6xl")}>{title}</h1>
         </div>
     );
 }
