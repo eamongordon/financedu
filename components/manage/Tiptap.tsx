77a04ac1750211bcd80ac2193eae6309c90c6246
Eamon Gordon <ekeokigordon@icloud.com>
2025-03-19 17:36:19 -0700

    feat: add menu items to tiptap editor

--- components/manage/Tiptap.tsx
+++ components/manage/Tiptap.tsx
@@ -3,13 +3,118 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
-
 export default function Tiptap() {
-  
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit.configure({
+        bulletList: {
+          HTMLAttributes: {
+            class: "list-disc ml-3",
+          },
+        },
+        orderedList: {
+          HTMLAttributes: {
+            class: "list-decimal ml-3",
+          },
+        },
+        heading: {
+          HTMLAttributes: {
+            class: "text-lg font-bold",
+          },
+        },
+      }),
+    ],
     content: '<p>Hello World! 🌎️</p>',
-  })
+    editorProps: {
+      attributes: {
+        class: 'prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none',
+      },
+    },
+    onUpdate: ({ editor }) => {
+      console.log(editor.getHTML());
+    },
+  });
 
-  return <EditorContent editor={editor} />
-};
+  return (
+    <div>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />;
+    </div>
+  );
+}
+
+import {
+  Bold,
+  Heading1,
+  Heading2,
+  Heading3,
+  Italic,
+  List,
+  ListOrdered,
+  Strikethrough,
+} from "lucide-react";
+import { Editor } from "@tiptap/react";
+import { Toggle } from '../ui/toggle'
+
+export function MenuBar({ editor }: { editor: Editor | null }) {
+  if (!editor) {
+    return null;
+  }
+
+  const Options = [
+    {
+      icon: <Heading1 className="size-4" />,
+      onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
+      preesed: editor.isActive("heading", { level: 1 }),
+    },
+    {
+      icon: <Heading2 className="size-4" />,
+      onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
+      preesed: editor.isActive("heading", { level: 2 }),
+    },
+    {
+      icon: <Heading3 className="size-4" />,
+      onClick: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
+      preesed: editor.isActive("heading", { level: 3 }),
+    },
+    {
+      icon: <Bold className="size-4" />,
+      onClick: () => editor.chain().focus().toggleBold().run(),
+      preesed: editor.isActive("bold"),
+    },
+    {
+      icon: <Italic className="size-4" />,
+      onClick: () => editor.chain().focus().toggleItalic().run(),
+      preesed: editor.isActive("italic"),
+    },
+    {
+      icon: <Strikethrough className="size-4" />,
+      onClick: () => editor.chain().focus().toggleStrike().run(),
+      preesed: editor.isActive("strike"),
+    },
+    {
+      icon: <List className="size-4" />,
+      onClick: () => editor.chain().focus().toggleBulletList().run(),
+      preesed: editor.isActive("bulletList"),
+    },
+    {
+      icon: <ListOrdered className="size-4" />,
+      onClick: () => editor.chain().focus().toggleOrderedList().run(),
+      preesed: editor.isActive("orderedList"),
+    }
+  ];
+
+  return (
+    <div className="border rounded-md p-1 mb-1 bg-background space-x-2 z-50">
+      {Options.map((option, index) => (
+        <Toggle
+          key={index}
+          pressed={option.preesed}
+          onPressedChange={option.onClick}
+        >
+          {option.icon}
+        </Toggle>
+      ))}
+    </div>
+  );
+}
