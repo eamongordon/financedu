74cce2fbaccb9fa02b013230b62d4f92f26d885d
Eamon Gordon <ekeokigordon@icloud.com>
2025-03-23 15:16:20 -0700

    refactor: add cursor pointer to footer logo link

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
                     <div>
-                        <Link href="/">
+                        <Link href="/" className='cursor-pointer'>
                             <Image height={50} width={200} src="/financedu-logo.svg" alt="Logo" />
                         </Link>
                         <p className="mt-6 font-medium">Follow Us!</p>
