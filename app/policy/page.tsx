fca31daa258f9df5ed978d3ebf22a11e3c2c11fd
Eamon Gordon <ekeokigordon@icloud.com>
2025-03-24 12:18:22 -0700

    fix: use a tag for contact email link

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -90,7 +90,7 @@ const Page = async () => {
                             <p>Have a suggestion for improving Financedu? Notice an error in our site? Need to contact us for another reason?
                                 <br />
                                 <br />
-                                Contact Us at: <Link className="font-semibold text-primary" href="mailto:info@financedu.org">info@financedu.org</Link>.
+                                Contact Us at: <a className="font-semibold text-primary" href="mailto:info@financedu.org">info@financedu.org</a>.
                             </p>
                         </div>
                     </section>
