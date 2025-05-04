fe38358dce0181cd0b4eb5f565b6f622747cb228
Eamon Gordon <ekeokigordon@icloud.com>
2025-05-04 12:11:43 -0700

    fix: mobile layout for fin AI notice

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,15 @@ export default function Home() {
   return (
     <>
       <Notice persistent>
-        <div className="h-full flex flex-row items-center justify-center text-sm gap-2 text-muted-foreground">
+        <div className="h-full flex flex-row items-center px-4 md:justify-center text-sm gap-2 text-muted-foreground">
           Introducing Fin, Our AI assistant!
           <Link
             href="/blog/introducing-fin-ai-assistant"
             target="_blank"
             className="font-semibold flex flex-row items-center justify-center"
           >
-            Read the announcement
+            <span className="hidden sm:block">Read the announcement</span>
+            <span className="block sm:hidden">Learn more</span>
             <ArrowRight
               size={16}
               className="ml-1"
