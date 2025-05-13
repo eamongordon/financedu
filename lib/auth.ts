0b7c422db6fd5ed8f3e0009c42b71cefaa2b1e99
Eamon Gordon <ekeokigordon@icloud.com>
2025-05-13 08:22:07 -0700

    feat: add user profile mapping for Google oauth

--- lib/auth.ts
+++ lib/auth.ts
@@ -44,6 +44,12 @@ export const auth = betterAuth({
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            mapProfileToUser: (profile) => {
+                return {
+                    firstName: profile.given_name,
+                    lastName: profile.family_name,
+                };
+            },
         },
         facebook: {
             clientId: process.env.FACEBOOK_CLIENT_ID as string,
