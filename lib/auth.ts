55f623e027cb866480596e7573fda1bb1b8e6930
Eamon Gordon <ekeokigordon@icloud.com>
2025-05-12 19:09:32 -0700

    feat: add additional fields to auth config

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,20 +4,34 @@ import { db } from "@/lib/db";
 import { headers } from "next/headers";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
-  }),
-  emailAndPassword: {
-    enabled: true
-  },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        usePlural: true
+    }),
+    user: {
+        additionalFields: {
+            firstName: {
+                type: "string"
+            },
+            lastName: {
+                type: "string"
+            },
+            roles: {
+                type: "string[]",
+            }
+        }
     },
-    facebook: {
-      clientId: process.env.FACEBOOK_CLIENT_ID as string,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
-    }
-  },
+    emailAndPassword: {
+        enabled: true
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+        facebook: {
+            clientId: process.env.FACEBOOK_CLIENT_ID as string,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+        }
+    },
 });
